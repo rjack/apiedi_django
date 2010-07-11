d898ece6044d7ae0e9e28b2bf5c7338f1cff011c
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-07-11 15:21:51 +0200

    metodi __unicode__ per Patient e Appointment

--- apiedi/models.py
+++ apiedi/models.py
@@ -12,8 +12,15 @@ class Patient(models.Model):
 	telephone = models.CharField(max_length=20, blank=True, null=True)
 	notes = models.CharField(max_length=500, blank=True, null=True)
 
+	def __unicode__(self):
+		return u"%s %s" % (self.name, self.surname)
+
+
 class Appointment(models.Model):
 	time = models.DateTimeField(primary_key=True)
 	visitType = models.CharField(max_length=1, choices=VISIT_TYPES)
 	patient = models.ForeignKey(Patient)
 	notes = models.CharField(max_length=500, blank=True, null=True)
+
+	def __unicode__(self):
+		return u"%s,%s,%s" % (self.time, self.patient, self.visitType)
