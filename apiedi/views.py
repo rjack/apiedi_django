135ced2ed4c06061e58e419e22f90fd954c4382a
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-07-12 11:37:08 +0200

    stub per le view `appointments' e `new_appointment'

--- apiedi/views.py
+++ apiedi/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def appointments(req):
+	return HttpResponse("Tutti gli appuntamenti")
+
+
+def new_appointment(req):
+	return HttpResponse("Nuovo appuntamento! Deve essere un POST.")
